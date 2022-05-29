64bd181e9c468365cddf2791597e36b3df015358
Andrew Howran <andrewhowrandev17@gmail.com>
2022-05-29 16:19:00 -0400

    fixed connection file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-  }
-);
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+      }
+    );
 
 module.exports = sequelize;
